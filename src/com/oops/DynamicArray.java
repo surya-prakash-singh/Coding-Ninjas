20d6c58d592159f3c2126e68a4f92a20c54f7c76
Surya P Singh <suryapsingh@L-156179489-SURYAPRAKASHSINGH.local>
2019-12-28 23:39:56 +0000

    oops-cdninja-ds-algo-dynamic-array

--- src/com/oops/DynamicArray.java
+++ src/com/oops/DynamicArray.java
@@ -23,17 +23,39 @@ public class DynamicArray {
 
     }
     public void set(int index, int element){
+      if(index>nextIndex){
+          return;
+      }else if(index == nextIndex){
+          add(element) ;
+      }else{
+          data[index] = element;
+      }
 
     }
+
     public int get(int index){
-    return 0;
+
+        if(index < nextIndex){
+            return  data[index];
+        }else{
+            throw new Error("Element at index doesnt exists");
+        }
     };
 
     public boolean isEmpty(){
+        if(nextIndex == 0){
+            return true;
+        }
         return false;
     }
     public int removeLast(){
-        return 0;
+        if(isEmpty()){
+            throw new Error("Element at index doesnt exists");
+        }
+        int value = data[nextIndex -1];
+        data[nextIndex - 1] = 0;
+            nextIndex--;
+        return value;
     }
     public void restructure(){
        int temp[] = data;
--- src/com/oops/DynamicArrayUse.java
+++ src/com/oops/DynamicArrayUse.java
@@ -7,6 +7,7 @@ public class DynamicArrayUse {
         for(int i = 0; i<100; i++){
             d.add(i+10);
         }
+
         System.out.println(d.size());
 
         d.set(4,10);
