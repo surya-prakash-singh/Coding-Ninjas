9ce43592b35ebe52b245b7f825e70a64b7d2096b
surya-prakash-singh <singh_suryaprakash@yahoo.com>
2020-01-01 23:44:40 +0000

    Recursion:CheckSorted

--- src/com/recursion/CheckSorted.java
+++ src/com/recursion/CheckSorted.java
@@ -22,10 +22,26 @@ public class CheckSorted {
         }else
             return false;
     }
+    public static boolean checkSorted2(int[] arr){
+        if(arr.length<=1){
+            return true;
+        }
+        if(arr[0]>arr[1]){
+            return false;
+        }
+        int smallInput[] = new int[arr.length -1];
 
+        for(int i=0;i<smallInput.length;i++){
+            smallInput[i] = arr[i+1];
+        }
+
+        boolean ans = checkSorted2(smallInput);
+        return ans;
+    }
     public static void main(String[] args) {
-        int input[] = {2,1,4,5,6};
+        int input[] = {4,5,2,3,15};
         int input2[] = {1,2,3,4,5,6};
         System.out.println(checkSorted(input2));
+        System.out.println(checkSorted2(input));
     }
 }
